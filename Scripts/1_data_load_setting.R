ea2dfe2a86ac60d414afd68896859c9eed000a48
Mirai <49144878+TorAdagio@users.noreply.github.com>
2021-10-26 13:28:34 +0200

    - Compounds dataset added - added log issues roporting - safetycopy original data

--- Scripts/1_data_load_setting.R
+++ Scripts/1_data_load_setting.R
@@ -1,20 +1,8 @@
 if(!require("pacman")){install.packages("pacman")}
 pacman::p_load(tidyverse, jsonlite )
 
-readLines("Data/odor_relations.json")
-test1 <- readLines("Data/odor_relations.json")
-test1
-
-test2<- str_remove_all(test1, pattern = '(?<=\\})\\,')
-test2
-read_json(test2)
-fromJSON(test2)
-str_extract_all(test2, pattern = '[:alnum:]' )
-
-read_json("Data/odor_relations.json" )
+#### Data importing  ------
+fromJSON("Data/odor_relations.json", flatten = T)
 
 
 
-json <- do.call(
-  rbind, 
-  lapply(paste(readLines("Data/odor_relations.json", warn=FALSE), collapse=""), jsonlite::fromJSON))
